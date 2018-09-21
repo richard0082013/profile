dba07e0a89f37fa08a6d5e56c669ce8af1ce8640
Tian Bo <rtian@twopicode.com.au>
2018-09-21 14:58:49 +1000

    text updates

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -31,7 +31,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Bo's Blog
           </Link>
         </h1>
       )
